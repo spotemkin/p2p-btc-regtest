f9da91d73d6702fb6e643e114cf5e6e4b3c281e5
Sergey Potemkin <68117087+spotemkin@users.noreply.github.com>
2023-12-21 02:53:09 +0300

    fix

--- bitcoin-generate-block.sh
+++ bitcoin-generate-block.sh
@@ -3,6 +3,6 @@
 while true; do
   echo "Generating a new block..."
   block_hash=$(/bitcoin/bin/bitcoin-cli -regtest -generate 1)
-  echo "$block_hash"
+  echo "$(date): Generated block hash: $block_hash" >> /bitcoin/bitcoin-block-gen.log
   sleep 60
 done
